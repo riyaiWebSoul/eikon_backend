f54097032e60f30338fab956dbeb471aaee0df44
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-20 14:34:49 +0530

    config remove

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const config = require("config");
-const { MongoClient } = require('mongodb');
+
 require('dotenv').config()
 const MONGODB_URI = process.env.MONGODB_URI;
 
