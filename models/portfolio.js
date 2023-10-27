6195959f288ad2a0bd5f75ce9612fd8c2ba78c11
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-27 15:51:37 +0530

    add more images schema

--- models/portfolio.js
+++ models/portfolio.js
@@ -4,6 +4,12 @@ const { Schema } = mongoose;
 const portfolioSchema = new Schema({
   images: [String],
   videos: [String],
+  logos:[{
+    logoImages:[String],
+    otherImages:[String],
+    iconImages:[String]
+}]
+  
 });
 
 exports.portfolio = mongoose.model("portfolio", portfolioSchema);
