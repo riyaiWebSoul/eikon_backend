9edcc23a8fe14cef3bd65b424557cfef36d163b8
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-30 12:04:36 +0530

    allow all method for login

--- routes/api/loginId.js
+++ routes/api/loginId.js
@@ -6,11 +6,11 @@ const loginIdController=require('../../controller/loginId');
 
 router
 .post('/',cors(),loginIdController.getAllloginIds)
-// .get('/',cors(),loginIdController.getAllloginIds)
-// .get('/:id',cors(),loginIdController.getloginId)
-// .put('/:id',cors(),loginIdController.replaceloginId)
-// .delete('/:id',cors(),loginIdController.deleteloginId)
-// .patch('/:id',cors(),loginIdController.updateloginId)
+.get('/',cors(),loginIdController.getAllloginIds)
+.get('/:id',cors(),loginIdController.getloginId)
+.put('/:id',cors(),loginIdController.replaceloginId)
+.delete('/:id',cors(),loginIdController.deleteloginId)
+.patch('/:id',cors(),loginIdController.updateloginId)
 
 
 
